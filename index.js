4f8e35079ceae9584b361feb3509dbe5b9e69527
Justin Dacquel <103160260+jdeeezzy@users.noreply.github.com>
2022-08-15 06:25:25 +0000

    add pages

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = "Our Home Page"
+  var heading = "Justin's Favorite Things"
+  res.render('pages/index',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Tennis', (req, res) => {
+  var title = "My favorite tennis player: Serena Williams"
+  var heading = 'My Website'
+  res.render('pages/tennis',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Shoes', (req, res) => {
+  var title = "Shoes, Shoes, Shoes!"
+  var heading = 'My Website'
+  res.render('pages/shoes',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Sushi', (req, res) => {
+  var title = "My Favorite Food: Sushi"
+  var heading = 'My Website'
+  res.render('pages/sushi',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
